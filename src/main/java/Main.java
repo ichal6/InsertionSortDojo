e41f5f246289e8c0d8efee9621bfd2cb50c6b943
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-05-09 12:32:52 +0200

    Add time mesarge

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        long timeRun = startTime();
         Integer[] numbers = {0};
         try {
             numbers = FileOperation.readFromFile("src/main/resources/one_thousand.txt");
@@ -12,6 +13,17 @@ public class Main {
         }
 //        for(Integer i: numbers)
 //        System.out.println(i);
+        System.out.println(String.format("time run: %.0f ms",stopTime(timeRun)));
+    }
 
+    private static long startTime(){
+        return System.nanoTime();
+    }
+
+    private static Double stopTime(long startTime){
+        long endTime = System.nanoTime();
+        Double start = Double.valueOf(startTime);
+        Double end = Double.valueOf(endTime);
+        return (end - start)/1000000.0;
     }
 }
