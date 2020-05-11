02d4f6d2b2c75aad7c3028f8f0c0fb6c06c64315
Michael Lechowicz <ichal.lechowicz@gmail.com>
2020-05-11 19:40:04 +0200

    Add new files

--- src/main/java/FileOperation.java
+++ src/main/java/FileOperation.java
@@ -1,11 +1,11 @@
 import java.io.*;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class FileOperation {
     public static Integer[] readFromFile(String filename) throws IOException {
-        Integer[] numbers;
         List<Integer> contentFromFile = new ArrayList<>();
 
         File file = new File(filename);
@@ -17,12 +17,40 @@ public class FileOperation {
         }
         br.close();
 
-        return numbers = contentFromFile.toArray(new Integer[0]);
+        return contentFromFile.toArray(new Integer[0]);
+    }
+
+    public static ArrayList<Integer> readFromFileAsArrayList(String filename) throws IOException {
+        ArrayList<Integer> contentFromFile = new ArrayList<>();
+
+        File file = new File(filename);
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        while (br.ready()){
+            contentFromFile.add(Integer.parseInt(br.readLine()));
+        }
+        br.close();
+
+        return contentFromFile;
+    }
+
+    public static LinkedList<Integer> readFromFileAsLinkedList(String filename) throws IOException {
+        LinkedList<Integer> contentFromFile = new LinkedList<>();
+
+        File file = new File(filename);
+
+        BufferedReader br = new BufferedReader(new FileReader(file));
+
+        while (br.ready()){
+            contentFromFile.add(Integer.parseInt(br.readLine()));
+        }
+        br.close();
+
+        return contentFromFile;
     }
 
     public static void saveToFile(String filename, Integer[] numbers) throws IOException{
-
-            String str = "Hello";
             BufferedWriter writer = new BufferedWriter(new FileWriter(filename));
             for(Integer number: numbers)
                 writer.write(String.format("%d\n", number));
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,10 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
+        withArray(args);
+    }
+
+    private static void withArray(String[] args){
         long timeRun = startTime();
         Integer[] numbers = {0};
         try {
